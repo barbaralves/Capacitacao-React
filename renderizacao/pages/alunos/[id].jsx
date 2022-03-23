6544c8c21198e9bda9df4c8e047cd25999a07716
Bárbara Alves <barbaraalves343@gmail.com>
2022-03-22 23:46:30 -0300

    Corrigindo renderização

--- renderizacao/pages/alunos/[id].jsx
+++ renderizacao/pages/alunos/[id].jsx
@@ -1,15 +1,19 @@
-export function getStaticPaths() {
+export async function getStaticPaths() {
+  const resp = await fetch(`http://localhost:3000/api/alunos/tutores`)
+  const ids = await resp.json()
+
+  const paths = ids.map(id => {
+    return { params: { id: `${id}` } }
+  })
+  
   return {
-    fallback: false, // se não encontrar o id na barra de navegação vai gerar um erro 404
-    paths: [
-      { params: { id: '7' } },
-      { params: { id: '9' } },
-      { params: { id: '3' } },
-    ]
+    fallback: true, // false = se não encontrar o id na barra de navegação vai gerar um erro 404
+    paths
   }
 }
 
 export async function getStaticProps({ params }) { //vai chamar a função 3 vezes, 3 paths
+  console.log(params)
   const resp = await fetch(`http://localhost:3000/api/alunos/${params.id}`)
   const aluno = await resp.json()
   
@@ -20,16 +24,19 @@ export async function getStaticProps({ params }) { //vai chamar a função 3 vez
   }
 }
 
-export default function AlunoPorId({props}) {
+export default function AlunoPorId(props) {
   const { aluno } = props
   return (
     <div>
       <h1>Detalhes do Aluno</h1>
-      <ul>
-        <li>{aluno.id}</li>
-        <li>{aluno.nome}</li>
-        <li>{aluno.ano}</li>
-      </ul>
+      {aluno ?
+        <ul>
+          <li>{aluno.id}</li>
+          <li>{aluno.nome}</li>
+          <li>{aluno.ano}</li>
+        </ul>
+        : false
+      }
     </div>
   )
 }
